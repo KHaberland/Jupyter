f1cc2d0ea610e5cf7af4bcfa6ef052b5c044b947
KHaberland <kris.haberland@gmail.com>
2023-03-20 18:35:35 +0200

    изменение функционала BeautiSoup на домашнее задание

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -39,3 +39,33 @@ print ("")
 
 for item in lst:
 	print (clean_item2(str(item)))
+
+
+url = "https://seaborn.pydata.org/examples/index.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+print(soup)
+
+lst = soup.find_all('p')
+for item in lst:
+	print (item)
+
+def clean_item(my_item):
+    position = my_item.find('</p')
+    return my_item[4: position]
+print (" ")
+
+for item in lst:
+    print (clean_item(str(item)))
+    
+lst = soup.find_all(class_ = 'thumb-label')
+for item in lst:
+	print (item)
+	
+def clean_item2(my_item):
+    position = my_item.find('</')
+    return my_item[30: position]
+print ("")
+
+for item in lst:
+	print (clean_item2(str(item)))
